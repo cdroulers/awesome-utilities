5853686e4cafdf7cb00a32929e9bbb49db7e352f
Christian Droulers <droulers.christian@gmail.com>
2011-09-27 14:47:00 -0400

    Adding unit tests for MvcHtmlStringExtensions

---
+++ Awesome.Utilities.Test/Web/GivenMvcHtmlString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace Awesome.Utilities.Test.Web
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class GivenMvcHtmlString
+    {
+        [Test]
+        public void When_adding_Then_concatenates()
+        {
+            var s = MvcHtmlString.Create("<p>lol</p>");
+
+            var actual = s.Add(MvcHtmlString.Create("<span>wat</span>"));
+
+            Assert.That(actual.ToString(), Is.EqualTo("<p>lol</p><span>wat</span>"));
+        }
+
+        [Test]
+        public void When_adding_with_space_Then_concatenates_with_space()
+        {
+            var s = MvcHtmlString.Create("<p>lol</p>");
+
+            var actual = s.Add(MvcHtmlString.Create("<span>wat</span>"), addSpace: true);
+
+            Assert.That(actual.ToString(), Is.EqualTo("<p>lol</p> <span>wat</span>"));
+        }
+
+        [Test]
+        public void When_adding_string_Then_concatenates()
+        {
+            var s = MvcHtmlString.Create("<p>lol</p>");
+
+            var actual = s.Add("<span>wat</span>");
+
+            Assert.That(actual.ToString(), Is.EqualTo("<p>lol</p><span>wat</span>"));
+        }
+
+        [Test]
+        public void When_adding_string_with_space_Then_concatenates_with_space()
+        {
+            var s = MvcHtmlString.Create("<p>lol</p>");
+
+            var actual = s.Add("<span>wat</span>", addSpace: true);
+
+            Assert.That(actual.ToString(), Is.EqualTo("<p>lol</p> <span>wat</span>"));
+        }
+    }
+}
--- Awesome.Utilities.Web.Mvc/MvcHtmlStringExtensions.cs
+++ Awesome.Utilities.Web.Mvc/MvcHtmlStringExtensions.cs
@@ -9,7 +9,12 @@ namespace System.Web.Mvc
     {
         public static MvcHtmlString Add(this MvcHtmlString self, MvcHtmlString other, bool addSpace = false)
         {
-            return MvcHtmlString.Create(self.ToString() + (addSpace ? " " : "") + other.ToString());
+            return self.Add(other.ToString(), addSpace);
+        }
+
+        public static MvcHtmlString Add(this MvcHtmlString self, string other, bool addSpace = false)
+        {
+            return MvcHtmlString.Create(self.ToString() + (addSpace ? " " : "") + other);
         }
     }
 }
