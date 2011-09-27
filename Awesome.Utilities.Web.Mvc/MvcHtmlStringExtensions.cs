374c3496b7fa7ebe7d760b7ef8c6b22bb3783be9
Christian Droulers <chris@invup.com>
2011-09-26 22:26:26 -0400

    IssueID #95 TimeSpan form and stuff

---
+++ Awesome.Utilities.Web.Mvc/MvcHtmlStringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Web.Mvc
+{
+    public static class MvcHtmlStringExtensions
+    {
+        public static MvcHtmlString Add(this MvcHtmlString self, MvcHtmlString other, bool addSpace = false)
+        {
+            return MvcHtmlString.Create(self.ToString() + (addSpace ? " " : "") + other.ToString());
+        }
+    }
+}
