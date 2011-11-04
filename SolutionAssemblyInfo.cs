7ca58bd4bbf1b5be9139105eb8aa6dff45b7fa19
Christian Droulers <droulers.christian@gmail.com>
2011-11-04 14:35:46 -0400

    Fixing assembly infos

---
+++ SolutionAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Christian Droulers and Mathieu Germain")]
+[assembly: AssemblyCopyright("Copyright © Christian Droulers and Mathieu Germain")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
