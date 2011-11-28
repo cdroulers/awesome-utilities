5e039414a804963b53090cd2f4c173a0e8e9bc75
Christian Droulers <droulers.christian@gmail.com>
2011-11-28 11:18:18 -0500

    Adding uri extensions

---
+++ Awesome.Utilities.System/UriExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    /// <summary>
+    ///     A set of extensions for the URI object.
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// Changes the query string for the specified new value.
+        /// </summary>
+        /// <param name="self">The self.</param>
+        /// <param name="newQueryString">The new query string, no need to put "?" in it.</param>
+        /// <returns></returns>
+        public static Uri ChangeQueryString(this Uri self, string newQueryString)
+        {
+            var builder = new UriBuilder(self);
+            builder.Query = newQueryString.TrimStart('?');
+            return builder.Uri;
+        }
+
+        /// <summary>
+        /// Changes the path for the specified new value.
+        /// </summary>
+        /// <param name="self">The self.</param>
+        /// <param name="newPath">The new query string, no need to put "?" in it.</param>
+        /// <returns></returns>
+        public static Uri ChangePath(this Uri self, string newPath)
+        {
+            var builder = new UriBuilder(self);
+            builder.Path = newPath.TrimStart('/');
+            return builder.Uri;
+        }
+    }
+}
---
+++ Awesome.Utilities.Test/System/GivenUriExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Awesome.Utilities.Test.System
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class GivenUriExtensions
+    {
+        [TestCase("http://example.org/folder?query=string", "lol=what", "http://example.org/folder?lol=what")]
+        [TestCase("http://example.org/folder?query=string", "?lol=what", "http://example.org/folder?lol=what")]
+        [TestCase("http://example.org/folder", "lol=what", "http://example.org/folder?lol=what")]
+        [TestCase("http://example.org/folder", "?lol=what", "http://example.org/folder?lol=what")]
+        public void When_changing_query_string_Then_returns_new_uri(string baseUri, string query, string expected)
+        {
+            var uri = new Uri(baseUri);
+            var actual = uri.ChangeQueryString(query);
+
+            Assert.That(actual, Is.EqualTo(new Uri(expected)));
+        }
+
+        [TestCase("http://example.org/folder?query=string", "folder/lol", "http://example.org/folder/lol?query=string")]
+        [TestCase("http://example.org/folder?query=string", "/folder/lol", "http://example.org/folder/lol?query=string")]
+        [TestCase("http://example.org/?query=string", "folder/lol", "http://example.org/folder/lol?query=string")]
+        [TestCase("http://example.org/?query=string", "/folder/lol", "http://example.org/folder/lol?query=string")]
+        public void When_changing_path_Then_returns_new_uri(string baseUri, string path, string expected)
+        {
+            var uri = new Uri(baseUri);
+            var actual = uri.ChangePath(path);
+
+            Assert.That(actual, Is.EqualTo(new Uri(expected)));
+        }
+    }
+}
