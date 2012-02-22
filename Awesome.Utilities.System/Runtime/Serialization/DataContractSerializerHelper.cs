177fbe46c608827911fe326d06ec72c3f006d54f
Christian Droulers <droulers.christian@gmail.com>
2012-02-22 17:13:22 -0500

    Adding a datacontract serializer helper

---
+++ Awesome.Utilities.System/Runtime/Serialization/DataContractSerializerHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace System.Runtime.Serialization
+{
+    /// <summary>
+    ///     Data Contract serializer helper
+    /// </summary>
+    public static class DataContractSerializerHelper
+    {
+        /// <summary>
+        /// Serializes the specified graph.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="graph">The graph.</param>
+        /// <param name="omitXmlDeclaration">if set to <c>true</c> [omit XML declaration].</param>
+        /// <param name="indent">if set to <c>true</c> [indent].</param>
+        /// <returns></returns>
+        public static string Serialize<T>(T graph, bool omitXmlDeclaration = false, bool indent = true)
+        {
+            var data = new DataContractSerializer(typeof(T));
+
+            using (var memory = new MemoryStream())
+            {
+                using (var writer = XmlWriter.Create(memory, new XmlWriterSettings
+                {
+                    OmitXmlDeclaration = omitXmlDeclaration,
+                    NamespaceHandling = NamespaceHandling.OmitDuplicates,
+                    Indent = indent
+                }))
+                {
+                    data.WriteObject(writer, graph);
+                }
+                memory.Seek(0, SeekOrigin.Begin);
+                using (var reader = new StreamReader(memory))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deserializes the specified graph.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml">The XML.</param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string xml)
+        {
+            var data = new DataContractSerializer(typeof(T));
+
+            using (var memory = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+            {
+                return (T)data.ReadObject(memory);
+            }
+        }
+    }
+}
---
+++ Awesome.Utilities.Test/Runtime/Serialization/GivenDataContractSerializerHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Runtime.Serialization;
+
+namespace Awesome.Utilities.Test.Runtime.Serialization
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class GivenDataContractSerializerHelper
+    {
+        [DataContract(Namespace = "")]
+        private class TestClass
+        {
+            [DataMember]
+            public string Value { get; set; }
+        }
+
+        [Test]
+        public void When_serializing_Then_works()
+        {
+            string result = DataContractSerializerHelper.Serialize(new TestClass() { Value = "Wot" });
+
+            Assert.That(result, Is.EqualTo(@"<?xml version=""1.0"" encoding=""utf-8""?>
+<GivenDataContractSerializerHelper.TestClass xmlns:i=""http://www.w3.org/2001/XMLSchema-instance"">
+  <Value>Wot</Value>
+</GivenDataContractSerializerHelper.TestClass>"));
+        }
+
+        [Test]
+        public void When_deserializing_Then_works()
+        {
+            var result = DataContractSerializerHelper.Deserialize<TestClass>(@"<GivenDataContractSerializerHelper.TestClass>
+  <Value>Wot</Value>
+</GivenDataContractSerializerHelper.TestClass>");
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Value, Is.EqualTo("Wot"));
+        }
+    }
+}
