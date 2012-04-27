37d03af8bc301d52cf4039cc6ae175117fbad60a
Christian Droulers <droulers.christian@gmail.com>
2012-04-27 15:50:33 -0400

    Fix test to work no matter the culture

--- Awesome.Utilities.Test/Runtime/Serialization/GivenAwfulSerializer.cs
+++ Awesome.Utilities.Test/Runtime/Serialization/GivenAwfulSerializer.cs
@@ -5,6 +5,7 @@ using System.Text;
 using NUnit.Framework;
 using System.Net.Mail;
 using System.Runtime.Serialization;
+using System.Globalization;
 
 namespace Awesome.Utilities.Test.Runtime.Serialization
 {
@@ -32,22 +33,24 @@ namespace Awesome.Utilities.Test.Runtime.Serialization
         [Test]
         public void When_serializing_Then_outputs_right_formatted_text()
         {
-            var serializer = new AwfulSerializer("  ").AddStringableType<MailAddress>();
-
-            string actual = serializer.Serialize(new TestClass()
+            using (Culture.As(CultureInfo.InvariantCulture))
             {
-                StringValue = "SOME STRING TEXT HERE",
-                IntValue = 1,
-                DateTimeValue = DateTime.Parse("2012-04-26 14:59:00"),
-                StringArray = new[] { "Text one", "Text two", "Text three" },
-                StringList = new List<string>() { "Text one", null, "Text three" },
-                Dictionary = new Dictionary<string, MailAddress>() { { "Clé one", new MailAddress("valueone@example.org") }, { "Clé two", new MailAddress("keytwo@example.org") }, { "Clé three", null } },
-                DictionaryComplex = new Dictionary<SecondClass, SecondClass>() { { new SecondClass() { One = "Clé one string" }, new SecondClass() { One = "Valeur one string" } } },
-                DictionaryComplex2 = new Dictionary<string, SecondClass>() { { "Clé one string", new SecondClass() { One = "Valeur one string" } } },
-            });
+                var serializer = new AwfulSerializer("  ").AddStringableType<MailAddress>();
 
-            Assert.That(actual, Is.Not.Null);
-            Assert.That(actual, Is.EqualTo(@"TestClass
+                string actual = serializer.Serialize(new TestClass()
+                {
+                    StringValue = "SOME STRING TEXT HERE",
+                    IntValue = 1,
+                    DateTimeValue = DateTime.Parse("2012-04-26 14:59:00"),
+                    StringArray = new[] { "Text one", "Text two", "Text three" },
+                    StringList = new List<string>() { "Text one", null, "Text three" },
+                    Dictionary = new Dictionary<string, MailAddress>() { { "Clé one", new MailAddress("valueone@example.org") }, { "Clé two", new MailAddress("keytwo@example.org") }, { "Clé three", null } },
+                    DictionaryComplex = new Dictionary<SecondClass, SecondClass>() { { new SecondClass() { One = "Clé one string" }, new SecondClass() { One = "Valeur one string" } } },
+                    DictionaryComplex2 = new Dictionary<string, SecondClass>() { { "Clé one string", new SecondClass() { One = "Valeur one string" } } },
+                });
+
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.EqualTo(@"TestClass
   StringValue (String) => SOME STRING TEXT HERE
   IntValue (Int32) => 1
   DateTimeValue (DateTime) => 2012-04-26 14:59:00
@@ -72,42 +75,49 @@ namespace Awesome.Utilities.Test.Runtime.Serialization
     Key  => Clé one string
     Value
       One (String) => Valeur one string"));
+            }
         }
 
         [Test]
         public void When_serializing_dictionary_Then_outputs_right_formatted_text()
         {
-            var serializer = new AwfulSerializer("  ");
-
-            string actual = serializer.Serialize(new Dictionary<string, SecondClass>()
+            using (Culture.As(CultureInfo.InvariantCulture))
             {
-                { "Clé one", new SecondClass() { One = "Second class valeur one" } },
-                { "Clé two", new SecondClass() { One = "Second class valeur two" } },
-            });
-            
-            Assert.That(actual, Is.Not.Null);
-            Assert.That(actual, Is.EqualTo(@"Dictionary<String, SecondClass>
+                var serializer = new AwfulSerializer("  ");
+
+                string actual = serializer.Serialize(new Dictionary<string, SecondClass>()
+                {
+                    { "Clé one", new SecondClass() { One = "Second class valeur one" } },
+                    { "Clé two", new SecondClass() { One = "Second class valeur two" } },
+                });
+
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.EqualTo(@"Dictionary<String, SecondClass>
   Key  => Clé one
   Value
     One (String) => Second class valeur one
   Key  => Clé two
   Value
     One (String) => Second class valeur two"));
+            }
         }
 
         [Test]
         public void When_enum_Then_outputs_right_formatted_text()
         {
-            var serializer = new AwfulSerializer("  ");
-
-            string actual = serializer.Serialize(new Dictionary<string, StringComparison>()
+            using (Culture.As(CultureInfo.InvariantCulture))
             {
-                { "Clé one", StringComparison.CurrentCulture },
-            });
+                var serializer = new AwfulSerializer("  ");
 
-            Assert.That(actual, Is.Not.Null);
-            Assert.That(actual, Is.EqualTo(@"Dictionary<String, StringComparison>
+                string actual = serializer.Serialize(new Dictionary<string, StringComparison>()
+                {
+                    { "Clé one", StringComparison.CurrentCulture },
+                });
+
+                Assert.That(actual, Is.Not.Null);
+                Assert.That(actual, Is.EqualTo(@"Dictionary<String, StringComparison>
   Clé one => CurrentCulture"));
+            }
         }
     }
 }
